a67a160937e44655447e2d716eada963cad169a3
AnastasiiaTsi <anastasiacikalo5@gmail.com>
2026-03-25 21:35:51 +0200

    modificated app.js: add second part of task

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { Command } = require("commander");
 const http = require("http");
 const fs = require("fs");
+const { XMLBuilder } = require("fast-xml-parser");
 
 const program = new Command();
 
@@ -18,7 +19,59 @@ if (!fs.existsSync(options.input)) {
 }
 
 const server = http.createServer((req, res) => {
-  res.end("Server works");
+  const url = req.url;
+  const showMF0 = url.includes("mf0=true");  
+  const showNormal = url.includes("normal=true");
+  
+  fs.readFile(options.input, "utf8", (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("Error reading file");
+      return;
+    }
+    
+    const banks = JSON.parse(data);
+    let filteredBanks = banks;
+    
+    if (showNormal) {
+      filteredBanks = [];
+      for (let i = 0; i < banks.length; i++) {
+        if (banks[i].COD_STATE === 1) {
+          filteredBanks.push(banks[i]);
+        }
+      }
+    }
+    
+    const banksXML = [];
+    for (let i = 0; i < filteredBanks.length; i++) {
+      const bank = filteredBanks[i];
+      
+      if (showMF0) {
+        banksXML.push({
+          mf0_code: bank.MFO,     
+          name: bank.NAME,
+          state_code: bank.COD_STATE
+        });
+      } else {
+        banksXML.push({
+          name: bank.NAME,
+          state_code: bank.COD_STATE
+        });
+      }
+    }
+    
+    const xmlObj = {
+      banks: {
+        bank: banksXML
+      }
+    };
+    
+    const builder = new XMLBuilder({ format: true });
+    const xml = builder.build(xmlObj);
+    
+    res.writeHead(200, { "Content-Type": "application/xml" });
+    res.end(xml);
+  });
 });
 
 server.listen(options.port, options.host, () => {
