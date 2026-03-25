17c6b908e2cb61d413805997a19387ca48000c4d
AnastasiiaTsi <anastasiacikalo5@gmail.com>
2026-03-25 21:25:47 +0200

    modificated app.js

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+const { Command } = require("commander");
 const http = require("http");
+const fs = require("fs");
+
+const program = new Command();
+
+program
+  .requiredOption("-i, --input <file>")
+  .requiredOption("-h, --host <host>")
+  .requiredOption("-p, --port <port>");
+
+program.parse(process.argv);
+const options = program.opts();
+
+if (!fs.existsSync(options.input)) {
+  console.error("Cannot find input file");
+  process.exit(1);
+}
 
 const server = http.createServer((req, res) => {
-  res.end("OK");
+  res.end("Server works");
 });
 
-server.listen(3000, "localhost", () => {
-  console.log("Server running");
+server.listen(options.port, options.host, () => {
+  console.log(`Server running at http://${options.host}:${options.port}`);
 });
